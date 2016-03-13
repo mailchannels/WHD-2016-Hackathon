c9c0eb67ae3df10c86b716eea70fc007cadb1aea
Constantin Romankiewicz <constantin@zweiiconkram.de>
2016-03-13 15:10:46 +0100

    Extracting domain from mail address.

--- plib/library/Receiver.php
+++ plib/library/Receiver.php
@@ -10,6 +10,18 @@ class Modules_Harvard_Receiver
 {
     public function processJson($data)
     {
-        var_dump($data);
+        if (isset($data['envelope_sender']))
+        {
+            $sender = $data['envelope_sender'];
+
+            if (preg_match('/(.*)\@(.*)/i', $data['envelope_sender'], $matches))
+            {
+                $username = $matches[1];
+                $domain   = $matches[2];
+
+                $mailSettings = new Modules_Harvard_MailSettings();
+                var_dump($mailSettings->disableMailDomain($domain));
+            }
+        }
     }
 }
