81fe7e555fc1c9639f405a7e59a5ac43534f5440
Mike Smith <mike@mdms.org>
2016-03-13 16:19:13 +0100

    Initial tabbed view with blocked list and configuration tabs

--- plib/controllers/IndexController.php
+++ plib/controllers/IndexController.php
@@ -2,7 +2,35 @@
 
 class IndexController extends pm_Controller_Action
 {
+    public function init() {
+        parent::init();
+        // Init title for all actions
+        $this->view->pageTitle = pm_Locale::lmsg('Harvard');
+        // Init tabs for all actions
+        $this->view->tabs = array(
+            array(
+                'title' => pm_Locale::lmsg('blocked'),
+                'action' => 'blocked'
+            ),
+            array(
+                'title' => pm_Locale::lmsg('configuration'),
+                'action' => 'configuration',
+            )
+        );
+    }
+
     public function indexAction()
     {
+        //echo Modules_Harvard_MailSettings::disableMailDomain('mailchannels.com');
+
+        $this->_forward('blocked');
+    }
+
+    public function blockedAction() {
+
+    }
+
+    public function configurationAction() {
+
     }
 }
