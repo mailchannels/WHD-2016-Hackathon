6df4947aebbd626324fe63b53d4ead20e774b846
Scott McKay <scottm@mailchannels.com>
2016-03-14 11:31:31 +0100

    Save list of blocked domains if it has changed on read.
    Everytime the list of blocked domains is checked, we check each of the
    domains to see if it has been enabled.  If any of them have been
    enabled, we remove them from the list, and re-save the list of domains.

--- plib/library/Receiver.php
+++ plib/library/Receiver.php
@@ -44,16 +44,17 @@ class Modules_Harvard_Receiver
         $mailSettings = new Modules_Harvard_MailSettings();
 
         foreach ($actionConfig as $item) {
-
-            pm_Log::debug("event: {$item['event']}, domain: $domain, action: {$item['action']}");
-
             if ($item['event'] == $event || $item['event'] == 'all') {
+
+                pm_Log::debug("condition: $item->condition_name, domain: $domain");
 
                 $mailSettings = new Modules_Harvard_MailSettings;
 
                 // TODO: Check for relevant settings.
                 $mailSettings->disableMailDomain($domain);
                 //$mailSettings->disableMailUser($domain, $username);
+
+                return;
             }
         }
     }
